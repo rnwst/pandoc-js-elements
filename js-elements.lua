387f6ace2920e8bee7c9e473c7b05a10ce37a67e
R. N. West <98110034+rnwst@users.noreply.github.com>
2025-05-20 00:20:00 +0100

    Implement source mapping
    This implementation uses a simple string search to find the
    corresponding input source positions. While this works well most of the
    time, it cannot always determine source positions unambiguously if the
    search string occurs more than once in the source file.
    
    Furthermore, only one input file is supported at this time.
    
    The input Markdown file is Base64 encoded and appended to the source
    file.
    
    Close #2.

--- js-elements.lua
+++ js-elements.lua
@@ -1,19 +1,20 @@
 -- <Utilities> ---------------------------------------------------------------------------------------------------------
 
----@type List<string>
-local html_formats = pandoc.List {
-   'chunkedhtml',
-   'html',
-   'html5',
-   'html4',
-   'slideous',
-   'slidy',
-   'dzslides',
-   'revealjs',
-   's5',
-}
+---Read file contents.
+---@param path string
+---@return string | nil
+local function read_file(path)
+   local file = io.open(path, 'r') -- "r" means read mode
+   if not file then
+      pandoc.log.warn('js-elements: Could not open file ' .. path)
+      return nil
+   end
+   local content = file:read('*a') -- "*a" reads the whole file
+   file:close()
+   return content
+end
 
----Write content to given file path.
+---Write given content to given file path.
 ---@param path string
 ---@param content string
 local function write_file(path, content)
@@ -156,6 +157,101 @@ local function url_decode(s)
    return decoded
 end
 
+---@type string  Base64 characters used for Base64 encoding
+local BASE64_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+---Encode a series of numbers into a VLQ Base64 string.
+---@param nums integer[]
+---@return string
+local function nums2VLQs(nums)
+   ---@type string[]
+   local vlqs = {}
+   for _, num in ipairs(nums) do
+      -- Convert number to VLQ format
+      local vlq = num < 0 and (-num << 1) + 1 or (num << 1)
+
+      local result = ''
+      repeat
+         local chunk = vlq & 31 -- Get the last 5 bits
+         vlq = vlq >> 5 -- Shift right by 5 bits
+         if vlq > 0 then
+            chunk = chunk | 32 -- Set the continuation bit if there are more groups
+         end
+         result = result .. BASE64_CHARS:sub(chunk + 1, chunk + 1)
+      until vlq == 0
+
+      table.insert(vlqs, result)
+   end
+   return table.concat(vlqs)
+end
+
+---Encode the given string in Base64.
+---@param str string
+local function base64_encode(str)
+   ---@type string[]
+   local base64 = {}
+
+   local pad_length = (3 - (#str % 3)) % 3
+   local padded_str = str .. string.char(0):rep(pad_length) -- pad with zeroes
+
+   ---@type fun(idx: integer): number
+   local function byte_at(idx) return padded_str:sub(idx, idx):byte() end
+
+   ---@type fun(num: integer)
+   local function insert_char(num) table.insert(base64, BASE64_CHARS:sub(num + 1, num + 1)) end
+
+   for i = 1, #padded_str, 3 do
+      local first, second, third = byte_at(i), byte_at(i + 1), byte_at(i + 2)
+      insert_char(first >> 2)
+      insert_char(((first & 3) << 4) + (second >> 4)) -- the two bit shifts...
+      insert_char(((second & 15) << 2) + (third >> 6)) -- ...need to sum to 8
+      insert_char(third & 63)
+   end
+   -- Now we have to deal with padding.
+   if pad_length > 0 then base64[#base64] = '=' end
+   if pad_length > 1 then base64[#base64 - 1] = '=' end
+   return table.concat(base64)
+end
+
+---Split a given string into lines.
+---@param str string
+---@return List<string>
+local function split_lines(str)
+   local lines = pandoc.List {}
+   local pos = 1
+
+   while true do
+      local start, finish = str:find('\r?\n', pos)
+      if not start then
+         -- Add the remaining text (could be empty).
+         lines:insert(string.sub(str, pos))
+         break
+      end
+      lines:insert(string.sub(str, pos, start - 1))
+      pos = finish + 1
+   end
+
+   return lines
+end
+
+---Escape a string according to the JSON specification so it can be used in a
+---JSON file.
+---@param str string
+---@return string
+local function json_escape(str)
+   local entities = {
+      ['"'] = '\\"',
+      ['\\'] = '\\\\',
+      ['\b'] = '\\b',
+      ['\f'] = '\\f',
+      ['\n'] = '\\n',
+      ['\r'] = '\\r',
+      ['\t'] = '\\t',
+   }
+   local escaped_str = str:gsub('["\\\b\f\n\r\t]', entities)
+   return escaped_str
+end
+
 -- </Utilities> --------------------------------------------------------------------------------------------------------
 
 ---@type boolean  Whether to execute this filter; overwritten by document front matter
@@ -176,9 +272,59 @@ local md_label_counter = 0
 ---@type List<Plain>
 local md_labels = pandoc.List {}
 
----@type List<string>
-local elements_js = pandoc.List {
-   [[
+-- Each JavaScript line becomes a separate item in this List.
+-- The `mappings` entry is a List of a List of integers, where the
+-- integer list each contains a source mapping with two fields:
+-- Output column and input string index.
+---@type List<{content: string, mappings: List<List<integer>>}>
+local elements_js = pandoc.List {}
+
+-- luacheck: ignore 631
+---@type string  Markdown source file content needed for source mapping, to be populated once we know the document contains JS elements
+local source = ''
+
+---@type integer  Last Markdown source file position (row and column), used for source mapping purposes
+local source_pos = 0
+
+---Add a generated JavaScript code block to the module.
+---A code block is a bunch of code that includes newlines.
+---Generated code is code that has no equivalent in the Markdown file and therefore isn't mapped.
+---@param js              string   JS code
+---@param start_new_line? boolean  whether to start a new line, defaults to `true`
+local function add_generated_js_block(js, start_new_line)
+   local lines = split_lines(js)
+   local loop_start_idx = 1
+   if start_new_line == false then
+      local last_entry = elements_js[#elements_js]
+      last_entry.content = last_entry.content .. lines[1]
+      loop_start_idx = 2
+   end
+   for i = loop_start_idx, #lines do
+      elements_js:insert { content = lines[i], mappings = pandoc.List {} }
+   end
+end
+
+---Add JavaScript code from the source to the module and update source map.
+---@param js              string   JS code
+---@param source_str_idx? integer  source string index of code for mapping
+---@param start_new_line? boolean  whether to start a new line, defaults to `true`
+local function add_mapped_line(js, source_str_idx, start_new_line)
+   local lines = split_lines(js)
+   if #lines > 1 then error('String passed to `add_mapped_line` contains at least one newline char!', 2) end
+   if start_new_line == false then
+      local last_entry = elements_js[#elements_js]
+      -- Source map text columns start at zero.
+      last_entry.mappings:insert(pandoc.List { #last_entry.content, source_str_idx })
+      last_entry.content = last_entry.content .. lines[1]
+   else
+      elements_js:insert {
+         content = lines[1],
+         mappings = pandoc.List { pandoc.List { 0, source_str_idx } },
+      }
+   end
+end
+
+add_generated_js_block([[
 // Header ----------------------------------------------------------------------
 
 'use strict';
@@ -224,8 +370,8 @@ const getMDLabel = (id) => {
     elt.removeAttribute('id');
     return elt;
 };
-]],
-}
+
+]])
 
 ---Create JS which retrieves Markdown label elements.
 ---@param elt (CodeBlock | Code | Image)
@@ -286,19 +432,24 @@ end
 ---@param placeholder_id string
 ---@param inline_js string
 local function insert_elt_js(label_defs_js, placeholder_id, inline_js)
-   elements_js:insert(string.format(
+   add_generated_js_block(string.format(
       [[
-
 /* *****************************************************************************
  * Element #%s */
 %s
-replacePlaceholder('%s', %s);
-]],
+replacePlaceholder('%s', ]],
       placeholder_elt_counter,
       label_defs_js,
-      placeholder_id,
-      inline_js
+      placeholder_id
    ))
+   local start, finish = source:find('`' .. inline_js .. '`', source_pos, true)
+   source_pos = finish or source_pos
+   if start then
+      add_mapped_line(inline_js, start + 1, false)
+   else
+      add_generated_js_block(inline_js, false)
+   end
+   add_generated_js_block(');\n\n', false)
 end
 
 ---Add code block JS to module.
@@ -309,9 +460,8 @@ local function code_block(elt)
       contains_js_elements = true
 
       code_block_counter = code_block_counter + 1
-      elements_js:insert(string.format(
+      add_generated_js_block(string.format(
          [[
-
 /* *****************************************************************************
  * Code block #%s */
 ]],
@@ -319,8 +469,25 @@ local function code_block(elt)
       ))
 
       local label_defs_js = create_md_labels(elt)
-      if label_defs_js ~= '' then elements_js:insert(label_defs_js) end
-      elements_js:insert(elt.text .. '\n')
+      if label_defs_js ~= '' then add_generated_js_block(label_defs_js) end
+
+      -- This is a very crude method to find the corresponding source position,
+      -- but it should work most of the time. Note that this method doesn't work
+      -- for indented code blocks.
+      -- If no source position is found, add the code block unmapped.
+      local start, finish = source:find(elt.text, source_pos, true)
+      if start then
+         source_pos = finish + 1
+         local lines = split_lines(elt.text)
+         add_mapped_line(lines[1], start)
+         for _, line in ipairs { table.unpack(lines, 2) } do
+            start = source:find('\r?\n', start + 1)
+            add_mapped_line(line, start + 1)
+         end
+      else
+         add_generated_js_block(elt.text)
+      end
+      add_generated_js_block('\n')
 
       if elt.attributes.include == 'true' then
          elt.classes:remove(table.pack(elt.classes:find('include'))[2])
@@ -409,12 +576,80 @@ local function image(elt)
    end
 end
 
-if html_formats:includes(FORMAT) or FORMAT:match('native') then
+---Construct source map.
+---@return string
+local function build_source_map()
+   local mappings_lines = pandoc.List {}
+   local prev_source_line = 0
+   local prev_source_column = 0
+   for _, entry in ipairs(elements_js) do
+      local vlqs = pandoc.List {}
+      local prev_output_column = 0
+      for _, mapping in ipairs(entry.mappings) do
+         local output_column = mapping[1]
+         local input_idx = mapping[2]
+         local source_line = ({ source:sub(1, input_idx - 1):gsub('\n', '') })[2]
+         local source_column = #(
+            ({ source:sub(1, input_idx - 1):find('\n([^\n]-)$') })[3] or source:sub(1, input_idx - 1)
+         )
+         -- Output column, source index, source line, source column
+         -- Output column, source line, and source column are relative to
+         -- their previous occurrences. Output column resets on each new line.
+         -- See https://tc39.es/ecma426/#sec-mappings
+         vlqs:insert(nums2VLQs {
+            output_column - prev_output_column,
+            0,
+            source_line - prev_source_line,
+            source_column - prev_source_column,
+         })
+         prev_output_column = output_column
+         prev_source_line = source_line
+         prev_source_column = source_column
+      end
+      mappings_lines:insert(table.concat(vlqs, ','))
+   end
+   local mappings = table.concat(mappings_lines, ';')
+   return ([[
+{
+  "version" : 3,
+  "file": "elements.js",
+  "sources": ["%s"],
+  "sourcesContent": ["%s"],
+  "mappings": "%s"
+}
+]]):format(pandoc.path.filename(PANDOC_STATE.input_files[1]), json_escape(source), mappings)
+end
+
+---Build the content of the file `js/elements.js`.
+---@return string
+local function build_elements_js()
+   local js = table.concat(elements_js:map(function(entry) return entry.content end), '\n')
+   return js .. '\n//# sourceMappingURL=data:application/json;base64,' .. base64_encode(build_source_map())
+end
+
+if
+   pandoc
+      .List({
+         'chunkedhtml',
+         'html',
+         'html5',
+         'html4',
+         'slideous',
+         'slidy',
+         'dzslides',
+         'revealjs',
+         's5',
+      })
+      :includes(FORMAT) or FORMAT:match('native')
+then
    ---@type Filter
    return {
       {
          Meta = function(meta)
-            if meta['js-elements'] == true then js_elements = true end
+            if meta['js-elements'] == true then
+               js_elements = true
+               source = read_file(PANDOC_STATE.input_files[1]) or ''
+            end
          end,
       },
       {
@@ -441,7 +676,7 @@ if html_formats:includes(FORMAT) or FORMAT:match('native') then
                local rel_path = 'js/elements.js'
                write_file(
                   pandoc.path.join { pandoc.path.directory(PANDOC_STATE.input_files[1]), rel_path },
-                  table.concat(elements_js, '\n')
+                  build_elements_js()
                )
 
                -- Add to header-includes for standalone documents.
